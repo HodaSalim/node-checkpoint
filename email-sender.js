36957cbbb9da92f1e8ea0650cfd85c93a42226f5
HodaSalim <hi@hodasalim.com>
2023-05-21 12:49:23 +0300

    chore: added email sender application using nodemailer

--- email-sender.js
+++ email-sender.js
@@ -1,1 +1,24 @@
-clea
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "hoda.salim11@gmail.com",
+    pass: "######",
+  },
+});
+
+var mailOptions = {
+  from: "hoda.salim11@gmail.com",
+  to: "hoda.salim11@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
